c3c13159c0e94b10c287cfade6fe934023419848
Asma <102598292+Ikeepmyideas@users.noreply.github.com>
2026-02-27 02:31:27 +0100

    Create init_db.py

---
+++ HealthTime/web/init_db.py
@@ -0,0 +1,34 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_URL = os.environ.get("DATABASE_URL")
+
+if not DB_URL:
+    print("Erreur : La variable DATABASE_URL n'a pas été trouvée.")
+    exit()
+
+try:
+    print("Connexion à la base de données...")
+    conn = psycopg2.connect(DB_URL)
+    cursor = conn.cursor()
+
+    print("Lecture du fichier schema.sql...")
+    # Assure-toi que le chemin correspond bien à ton dossier
+    with open("database/schema.sql", "r", encoding="utf-8") as file:
+        sql_script = file.read()
+
+    print("Création des tables en cours...")
+    cursor.execute(sql_script)
+    conn.commit()
+
+    print("Succès ! Toutes les tables ont été créées avec succès !")
+
+except Exception as e:
+    print(f"Erreur lors de l'exécution : {e}")
+
+finally:
+    if 'cursor' in locals(): cursor.close()
+    if 'conn' in locals(): conn.close()
